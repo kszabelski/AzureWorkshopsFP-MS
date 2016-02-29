4c0675b7223b623b113fdb85fa73104e6f3aa7f0
Krzysztof Szabelski <kszabelski@gmail.com>
2016-02-29 11:37:46 +0100

    Template MVC app

---
+++ AzureConstructionsProgressTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureConstructionsProgressTracker.Startup))]
+namespace AzureConstructionsProgressTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
